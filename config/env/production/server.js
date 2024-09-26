00b314cebd6d4a20724114b1bee457d41e456cec
willyam ribiere <willyamrib@gmail.com>
2024-09-26 15:54:55 +0200

    Update database configuration

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 // Path: ./config/env/production/server.js`
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   proxy: true,
-  url: env("MY_HEROKU_URL"),
+  url: env("MY_HEROKU_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
   },
