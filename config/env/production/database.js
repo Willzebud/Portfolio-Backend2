55a1a1d580b0b35571a81a811484666d3ee6c929
willyam ribiere <willyamrib@gmail.com>
2024-09-26 15:50:36 +0200

    Update database configuration with hardcoded values

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,17 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "clhtb6lu92mj2.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com"
-      ),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "dct63fh5nej8jp"),
-      user: env("DATABASE_USERNAME", "ueibs0ann1qbuu"),
-      password: env(
-        "DATABASE_PASSWORD",
-        "pc917933459e298636b4537d128d5c2d4b5f6e8c994ffaa5758e680ff9d2f3b95"
-      ),
+      host: "cah8ha8ra8h8i7.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com",
+      port: 5432,
+      database: "dadjh5juvfpd2g",
+      user: "u12ucegkm96488",
+      password:
+        "p641c0afcdbf287787836bf70f1cd63bd4ca7ce9e44b98a7c717ee85e844c30af",
       ssl: {
         rejectUnauthorized: false,
       },
